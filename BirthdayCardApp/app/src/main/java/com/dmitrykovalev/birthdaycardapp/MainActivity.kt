fb8b3f44b18f2d7f7ce37b59188a585b30b5c54e
dmko1610 <trigger1610@gmail.com>
2023-12-29 15:38:15 +0400

    Add styling

--- BirthdayCardApp/app/src/main/java/com/dmitrykovalev/birthdaycardapp/MainActivity.kt
+++ BirthdayCardApp/app/src/main/java/com/dmitrykovalev/birthdaycardapp/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,7 +60,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
     Box {
-        Image(painter = image, contentDescription = null)
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop, alpha = 0.5F)
         GreetingText(
             message = message,
             from = from,
