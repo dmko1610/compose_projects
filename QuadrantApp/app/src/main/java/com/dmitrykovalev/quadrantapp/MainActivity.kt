70648237e85111fefb8da15155b8d60c27c4cb09
dmko1610 <trigger1610@gmail.com>
2023-12-29 16:48:21 +0400

    Add layout

--- QuadrantApp/app/src/main/java/com/dmitrykovalev/quadrantapp/MainActivity.kt
+++ QuadrantApp/app/src/main/java/com/dmitrykovalev/quadrantapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.dmitrykovalev.quadrantapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dmitrykovalev.quadrantapp.ui.theme.QuadrantAppTheme
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Quadrant()
                 }
             }
         }
@@ -30,17 +34,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Quadrant(modifier: Modifier = Modifier) {
+    Column {
+        Row(modifier = modifier.weight(1f, false)) {
+            QuadrantCard(
+                title = "Text Composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                modifier = modifier.weight(1f)
+            )
+            QuadrantCard(
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                modifier = modifier.weight(1f)
+            )
+        }
+        Row(modifier = modifier.weight(1f, false)) {
+            QuadrantCard(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                modifier = modifier.weight(1f)
+            )
+            QuadrantCard(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                modifier = modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun QuadrantCard(title: String, description: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = title)
+        Text(text = description)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuadrantAppTheme {
-        Greeting("Android")
+        Quadrant()
     }
 }
