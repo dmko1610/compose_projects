e85ef14af791c95dadc7eb928ec994077d5223fb
dmko1610 <trigger1610@gmail.com>
2023-12-29 16:12:33 +0400

    Add styling Extract string resources

--- LearnTogetherApp/app/src/main/java/com/dmitrykovalev/learntogetherapp/MainActivity.kt
+++ LearnTogetherApp/app/src/main/java/com/dmitrykovalev/learntogetherapp/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dmitrykovalev.learntogetherapp.ui.theme.LearnTogetherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,15 +41,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TutorialHeaderImage(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.bg_compose_background)
-    Image(painter = image, contentDescription = null)
-    TutorialTitleText(modifier = modifier)
+    Column {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.FillWidth)
+        TutorialTitleText(modifier = modifier)
+    }
 }
 
 @Composable
 fun TutorialTitleText(modifier: Modifier = Modifier) {
-    Text(text = "Jetpack Compose tutorial", modifier = modifier)
-    Text(text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.")
-    Text(text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
+    Column {
+        Text(
+            text = stringResource(R.string.tutorial_text),
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.tutorial_description_text),
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp, 0.dp, 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.tutorial_full_description_text),
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
