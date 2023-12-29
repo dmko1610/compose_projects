46c30d6f9590a489d25548b57932f533854da1d3
dmko1610 <trigger1610@gmail.com>
2023-12-29 18:25:19 +0400

    ADd basic layout

--- BusinessCardApp/app/src/main/java/com/dmitrykovalev/businesscardapp/MainActivity.kt
+++ BusinessCardApp/app/src/main/java/com/dmitrykovalev/businesscardapp/MainActivity.kt
@@ -3,12 +3,30 @@ package com.dmitrykovalev.businesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dmitrykovalev.businesscardapp.ui.theme.BusinessCardAppTheme
 
@@ -22,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard(name = "Dmitry Kovalev", position = "Software Engineer")
                 }
             }
         }
@@ -30,17 +48,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCard(name: String, position: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.android_logo)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                contentScale = ContentScale.Inside,
+                modifier = modifier.background(Color(0xFF00000b))
+            )
+            Text(text = name)
+            Text(text = position)
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                Icon(Icons.Default.Call, contentDescription = "Phone number")
+                Text("(+7) 937-659-63-56")
+            }
+            Row {
+                Icon(Icons.Default.Person, contentDescription = "Social media")
+                Text("@dmitrykovalev")
+            }
+            Row {
+                Icon(Icons.Default.Email, contentDescription = "Email")
+                Text("trigger1610@gmail.com")
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
-        Greeting("Android")
+        BusinessCard("Solaire Of Astora", "Knight of White")
     }
 }
