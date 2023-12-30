9586f6834c12b199f57ea645dec2336518875bd2
dmko1610 <trigger1610@gmail.com>
2023-12-30 15:07:43 +0400

    Add dice images

--- DiceRollerApp/app/src/main/java/com/dmitrykovalev/diceroller/MainActivity.kt
+++ DiceRollerApp/app/src/main/java/com/dmitrykovalev/diceroller/MainActivity.kt
@@ -3,16 +3,21 @@ package com.dmitrykovalev.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dmitrykovalev.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +34,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(R.drawable.dice_1), contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
